f21980a5317e76a128f5f64787b118c3d53bbc54
ShniderXPRO <65216448+ShniderXPRO@users.noreply.github.com>
2021-04-11 13:49:23 -0500

    Add files via upload

---
+++ pubgcrack.py
@@ -0,0 +1,19 @@
+import os
+import time
+os.system("clear")
+time.sleep(2)
+os.system("figlet PUBG CRACK")
+time.sleep(1)
+xoo = input ("Enter ID: ")
+time.sleep(1)
+print ("\033[0;33mPlease Wait....")
+time.sleep(3)
+print ("\033[1;32mSearching")
+time.sleep(9999)
+time.sleep(99999)
+time.sleep(12313)
+time.sleep(333)
+time.sleep(333)
+time.sleep(999)
+time.sleep(990)
+exit
